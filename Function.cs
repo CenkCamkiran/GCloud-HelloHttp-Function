1f5c2097a819d73b425a84589b79f6d70c1c1f1a
Cenk Camkıran <34924560+CenkCamkiran@users.noreply.github.com>
2023-01-21 14:33:55 +0300

    gcloud funcs docs added

--- Function.cs
+++ Function.cs
@@ -1,19 +1,46 @@
 using Google.Cloud.Functions.Framework;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System.IO;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace HelloFunctions
 {
     public class Function : IHttpFunction
     {
-        /// <summary>
-        /// Logic for your function goes here.
-        /// </summary>
-        /// <param name="context">The HTTP context, containing the request and the response.</param>
-        /// <returns>A task representing the asynchronous operation.</returns>
+        private readonly ILogger _logger;
+
+        public Function(ILogger<Function> logger) =>
+            _logger = logger;
+
         public async Task HandleAsync(HttpContext context)
         {
-            await context.Response.WriteAsync("Hello, Functions Framework.");
+            HttpRequest request = context.Request;
+            // Check URL parameters for "message" field
+            string message = request.Query["message"];
+
+            // If there's a body, parse it as JSON and check for "message" field.
+            using TextReader reader = new StreamReader(request.Body);
+            string text = await reader.ReadToEndAsync();
+            if (text.Length > 0)
+            {
+                try
+                {
+                    JsonElement json = JsonSerializer.Deserialize<JsonElement>(text);
+                    if (json.TryGetProperty("message", out JsonElement messageElement) &&
+                        messageElement.ValueKind == JsonValueKind.String)
+                    {
+                        message = messageElement.GetString();
+                    }
+                }
+                catch (JsonException parseException)
+                {
+                    _logger.LogError(parseException, "Error parsing JSON request");
+                }
+            }
+
+            await context.Response.WriteAsync(message ?? "Hello World!");
         }
     }
 }
